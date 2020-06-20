1f656d1144b23027a81897c058f68eb609d13bda
JeremyTo95 <jeremy.tourari@gmail.com>
2020-06-20 15:04:12 +0200

    patch socket exec

--- src/back/server/full/ExecClient.java
+++ src/back/server/full/ExecClient.java
@@ -4,8 +4,13 @@ import front.model.Constants;
 import front.model.Message;
 import front.model.User;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class ExecClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Client client = new Client();
         client.connect(Constants.IP_SERVER);
 
@@ -14,7 +19,15 @@ public class ExecClient {
         Message message = new Message(user.getId(), "Hello (from Jérémy)");
         Message message2 = new Message(user2.getId(), "Hello (from Sofiane)");
 
-//        client.sendMessage(message);
-//        client.sendMessage(message2);
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.print("> ");
+            String command = keyboard.readLine();
+            if (command.equals("quit")) break;
+            if (command.startsWith("send ")) {
+                client.sendMessage(Constants.IP_SERVER, new Message(user.getId(), command.replace("send ", "")));
+            }
+        }
     }
 }
